5f436e99956ad891246a97e79f6a4a44d6bbb2a5
Андрей Беляков <ttxiom@gmail.com>
2022-11-09 13:26:19 +0500

    lect 2022 11 09

--- part1/lections/lect10/03.py
+++ part1/lections/lect10/03.py
@@ -18,7 +18,28 @@ def get_map(num):
     return sum(map(int, str(num)))
     
 
+def get_gen(num):
+    lst = list(str(num))
+    return sum([int(smb) for smb in lst])
+
+
+def get_filter(num):
+    """ найти сумму нечётных элементов """
+    res = 0
+    while num > 0:
+        ost = num % 10
+        res += ost if ost % 2 > 0 else 0
+        num //= 10
+    return res
+
+
+# обычным циклом
+# через filter
+# через generator
+
+
 num = 123045
 print(get_ariphm(num))
 print(get_str(num))
 print(get_map(num))
+print(get_gen(num))

c5b8bb798cb97f2011498f80f60925c38a0547e9
Андрей Беляков <ttxiom@gmail.com>
2023-03-27 13:18:17 +0500

    Update 02.py

--- part2/lections/lect06-bs4/02-pages/02.py
+++ part2/lections/lect06-bs4/02-pages/02.py
@@ -1,16 +1,7 @@
 from bs4 import BeautifulSoup
 import json
-import requests
 from time import sleep
-
-
-def get_html(url):
-    head = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0'
-    }
-    resp = requests.get(url, headers=head)
-    resp.encoding = "utf8"
-    return resp.text
+from parsing import get_html
 
 
 def get_books(html):

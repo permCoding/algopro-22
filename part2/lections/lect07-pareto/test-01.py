57471383be64cd5ecd18adc0467832238ac19d01
Андрей Беляков <ttxiom@gmail.com>
2023-04-02 19:40:56 +0500

    up

---
+++ part2/lections/lect07-pareto/test-01.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+ 
+x = [1, 2, 3, 4]
+y = [4, 1, 3, 6]
+plt.scatter(x, y, c='green')
+ 
+x = [5, 6, 7, 8]
+y = [1, 3, 5, 2]
+plt.scatter(x, y, c='red')
+
+plt.show()
